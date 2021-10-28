7f8404cf266ac4cc437e870c76ea47457b15b7cd
xin-w <ivisionary@163.com>
2021-10-28 23:37:17 +0800

    Solved 26. Remove Duplicates from Sorted Array.

--- leetcode_xw001/main.cpp
+++ leetcode_xw001/main.cpp
@@ -5,9 +5,11 @@
 #include <iostream>
 
 #include "Container_With_Most_Water.h"
+#include "Remove_Duplicates_from_Sorted_Array.h"
 
 int main() {
   ContainerWithMostWater::test();
+  RemoveDuplicatesfromSortedArray::test();
 
   std::cout << "All tests passed" << std::endl;
   return 0;
